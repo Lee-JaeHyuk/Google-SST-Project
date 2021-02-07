4212c660782a8b577b57c1011520c60da5445fba
LeeJaeHyuk <leeseap33@naver.com>
2021-02-07 20:55:48 +0900

    commit

---
+++ GoogleCloudAPITest/WinformSpeechAPI/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System.Security;
+
+namespace WinformSpeechAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtTest = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtTest
+            // 
+            this.TxtTest.Location = new System.Drawing.Point(13, 13);
+            this.TxtTest.Multiline = true;
+            this.TxtTest.Name = "TxtTest";
+            this.TxtTest.Size = new System.Drawing.Size(762, 503);
+            this.TxtTest.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(523, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(85, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 330);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(429, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(273, 330);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(0, 0);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(0, 0);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(407, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(0, 0);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(101, 319);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(0, 0);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.Location = new System.Drawing.Point(247, 319);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(0, 0);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 528);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtTest;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ GoogleCloudAPITest/WinformSpeechAPI/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Google.Cloud.Speech.V1;
+
+//Install-Package Google.Cloud.Speech.V1 -Version 1.3.1
+
+namespace WinformSpeechAPI
+{
+    public partial class Form1 : Form
+    {
+        List<string> teller = new List<string>();
+        List<string> fruit = new List<string>(new string[] { "사과", "배" });
+        List<string> ve = new List<string>(new string[] { "기차", "자전거" });
+        List<string> ca = new List<string>(new string[] { "시계", "자" });
+
+        //List<string> f_common = new List<string>(new string[] { "과일 입니다", "동그랗다", "빨갛다", "먹을 수 있습니다", "둥급니다", "맛있습니다", "과즙이 많습니다", "제배가 가능합니다", "키울 수 있습니다", "비닐하우스에서 제배합니다" });
+        List<string> f_common = new List<string>(new string[] { "과일", "동그람", "맛있다", "과즙" ,"먹는것", "과즙", "재배가능"});
+        List<string> v_common = new List<string>(new string[] { "탈것", "운송수단", "여행을 위한 것", "여행", "바퀴가 있다", "바퀴", "굴러간다" });
+        List<string> c_common = new List<string>(new string[] { "측량도구", "무언갈 재는것", "숫자가 있다", "눈금이 있다", "숫자", "눈금" });
+        int ct = 0;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        float cot;
+        string answer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initTimer();
+            Credetials();   // 구글 Credentials 인증 진행
+            //QuickStart();
+            textBox1.Text = "사과와 배의 공통점은 무엇인가요?";
+            pictureBox1.Load(@"D:\apple.PNG");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Load(@"D:\pear.PNG");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            _ = StreamingMicRecognizeAsync(3);
+            timer.Start();
+        }
+
+        public void initTimer()
+        {
+            timer.Interval = TimeSpan.FromSeconds(4);
+            timer.Tick += Timer_Tick;
+
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            if (ct == 0)
+            {
+                button1.PerformClick();
+            }
+            else if (ct == 1)
+            {
+                button2.PerformClick();
+            }
+            else if (ct == 2)
+            {
+                button3.PerformClick();
+            }
+            else if(ct == 3)
+            {
+                button4.PerformClick();
+            }
+            else if(ct == 4)
+            {
+                button5.PerformClick();
+            }
+            else
+            {
+                timer.Stop();
+                clear1();
+            }
+
+            ct++;
+        }
+
+        #region ** 구글 클라우드 인증관련 **
+        public static string Credentials_File = "wide-link-259109-47d68c2646dc.json";
+        private void Credetials()
+        {
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", Credentials_File);
+        }
+        #endregion
+        #region ** StreamingRecognize 관련 **
+        string[] RecommendedWord = new string[] { "사자", "하마" };
+
+        public void compare1()
+        {
+            int flag = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (answer == f_common[i])
+                {
+                    textBox1.Text = "맞았습니다";
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                textBox1.Text = "틀렸습니다";
+            }
+
+        }
+
+        public void compare2()
+        {
+            int flag = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (answer == v_common[i])
+                {
+                    textBox1.Text = "맞았습니다";
+                    flag = 1;
+                }
+
+            }
+            if (flag == 0)
+            {
+                textBox1.Text = "틀렸습니다";
+            }
+
+        }
+
+        public void compare3()
+        {
+            int flag = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                if (answer == c_common[i])
+                {
+                    textBox1.Text = "맞았습니다";
+                    flag = 1;
+                }
+
+            }
+            if (flag == 0)
+            {
+                textBox1.Text = "틀렸습니다";
+            }
+
+        }
+
+        public void clear1()
+        {
+            pictureBox1.Load(@"D:\수고.PNG");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Load(@"D:\많으셨습니다.PNG");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            textBox1.Text = ""; 
+
+        }
+
+
+        public async Task<object> StreamingMicRecognizeAsync(int seconds)
+        {
+            //TextBox textBox = txt;
+            //form1.InitializeComponent();
+
+            SpeechClient speech = SpeechClient.Create();
+            SpeechClient.StreamingRecognizeStream streamingCall = speech.StreamingRecognize();
+
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 16000,
+                            LanguageCode = "ko-KR",
+                            SpeechContexts = { new SpeechContext { Phrases = { RecommendedWord } } } // 힌트 
+                        },
+                        InterimResults = true,
+                    }
+                });
+            
+            Task printResponses = Task.Run(async () =>
+            { 
+                while (await streamingCall.ResponseStream.MoveNext(default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream.Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            if (this.InvokeRequired)
+                            {
+                                //this.Invoke(
+                                //    new MethodInvoker(
+                                //    delegate () { this.TxtTest.AppendText(alternative.Transcript + "\r\n"); }));
+
+                                this.Invoke(
+                                    new MethodInvoker(
+                                    delegate () { 
+                                        this.TxtTest.AppendText(
+                                       // alternative.Transcript + " / " +
+                                       // alternative.Confidence.ToString() + " / " + 
+                                       // alternative.Words + " / " +
+                                        "\r\n"); 
+                                    })); // 띄어 쓰는건 이부분이니까
+                                
+                                answer = alternative.Transcript;
+                                cot = 1;
+                            }
+                        }
+
+                    } 
+
+                }
+            }); 
+
+          
+            object writeLock = new object();
+            bool writeMore = true;
+            var waveIn = new NAudio.Wave.WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            waveIn.DataAvailable +=
+                (object sender, NAudio.Wave.WaveInEventArgs args) =>
+                {
+                    lock (writeLock)
+                    {
+                        if (!writeMore)
+                        {
+                            return;
+                        }
+
+                        streamingCall.WriteAsync(
+                            new StreamingRecognizeRequest()
+                            {
+                                AudioContent = Google.Protobuf.ByteString.CopyFrom(args.Buffer, 0, args.BytesRecorded)
+                            }).Wait();
+                    }
+                };
+            waveIn.StartRecording();
+            TxtTest.Text += "Speak now."; // 콘솔창 
+            Console.WriteLine("Speak now."); 
+            await Task.Delay(TimeSpan.FromSeconds(seconds)); 
+            waveIn.StopRecording(); 
+            lock (writeLock) 
+            {
+                writeMore = false;
+            } // 이부분이 무슨 일을 하는걸가
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+        #endregion
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            compare1();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "자, 그럼 기차와 자전거의 공통점은 무엇일까요?";
+            pictureBox1.Load(@"D:\train.PNG");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Load(@"D:\by.PNG");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            _ = StreamingMicRecognizeAsync(5);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            compare2();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "자, 그럼 시계와 자의 공통점은 무엇일까요?";
+            pictureBox1.Load(@"D:\시계.PNG");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Load(@"D:\자.PNG");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            _ = StreamingMicRecognizeAsync(5);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            compare3();
+        }
+    }
+}
